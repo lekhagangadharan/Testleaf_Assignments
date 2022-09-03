ce33a2654d18195c64ef012c4155d56ba01d9280
lekhagangadharan <78950627+lekhagangadharan@users.noreply.github.com>
2022-09-04 00:18:43 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Fibonacci {
+	
+	public static void main(String[] args) {
+		
+		int n=11;
+		int a=0;
+		int b=1;
+		int sum=0;
+		System.out.print(a + "," + b);
+		
+		for (int i = 1; i <= n; i++) {
+			
+			sum = a+b;
+			System.out.print("," + sum);
+			a=b;
+			b=sum;
+			
+		}
+		
+	}
+
+}
